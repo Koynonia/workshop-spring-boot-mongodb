3fd4ff904ebaccdca434e4ab68270a96334f384c
koynonia <koynonia@gmail.com>
2018-09-10 11:47:13 -0300

    Consulta simples com query methods

--- src/main/java/br/com/linoox/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/linoox/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package br.com.linoox.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import br.com.linoox.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	// Usando os query methods do Spring Boot com IgnoreCase
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
