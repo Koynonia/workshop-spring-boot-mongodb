0fc8959dae701f0f3368420a91df40e139deaef4
koynonia <koynonia@gmail.com>
2018-09-10 12:00:28 -0300

    Consulta simples com @Query

--- src/main/java/br/com/linoox/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/linoox/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,17 @@ package br.com.linoox.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.linoox.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	// Usando os query methods do Spring Boot com IgnoreCase
+	// Consulta simples com @Query do Spring Boot
+	@Query("{'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
+	// Consulta simples com query methods do Spring Boot com IgnoreCase
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
